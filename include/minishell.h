1a7aca64243163bef81061d5653358c82e129cc2
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-09-01 11:58:31 -0400

    Added includes for allowed functions

---
+++ include/minishell.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chughes <chughes@student.42quebec.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/01 11:30:00 by chughes           #+#    #+#             */
+/*   Updated: 2022/09/01 11:30:00 by chughes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+// STDLIB Includes
+# include <unistd.h> // write, access, read, close, fork, getcwd, chdir, unlink, execve, dup, dup2, pipe, isatty, ttyname, ttyslot
+# include <stdio.h> // readline, printf, perror
+# include <stdlib.h> // malloc, free, exit, getenv, 
+# include <readline/readline.h> // readline
+# include <readline/history.h> // readline
+# include <fcntl.h> // open
+# include <sys/wait.h> // wait, wait3, wait4
+# include <sys/types.h> // waitpid, wait3, opendir, closedir
+# include <sys/wait.h> // waitpid
+# include <sys/time.h> // wait3
+# include <sys/resource.h> // wait3
+# include <signal.h> // signal, sigaction, sigemptyset, sigaddset, kill
+# include <sys/stat.h> // stat, lstat, fstat
+# include <dirent.h> // opendir, readdir, closedir
+# include <string.h> // strerror
+# include <sys/ioctl.h> // ioctl
+# include <termios.h> // tcsetattr, tcgetattr
+# include <curses.h> // tgetent, tgetflag, tgetnum, tgetstr, tgoto, tputs
+# include <term.h> // tgetent, tgetflag, tgetnum, tgetstr, tgoto, tputs
+
+#endif
