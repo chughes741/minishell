b29204fca2318ec6c1a3bda65d9da036dd3a3b86
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-09-01 13:24:20 -0400

    Added some basic promts to sh

--- src/minishell.c
+++ src/minishell.c
@@ -14,5 +14,17 @@
 
 int	main()
 {
+	char	*cmd;
+
+	cmd = ft_strdup("Welcome to Marc and Cole's minishell");
+	while (ft_strncmp(cmd, "exit", 4))
+	{
+		if (!ft_strncmp(cmd, "^D", 2))
+			exit(0);
+		//TODO Handle \\ and ^C exits, might need to use .inputrc and ENV for this
+		printf("%s\n", cmd);
+		free(cmd);
+		cmd = readline("> ");
+	}
 	return (0);
 }
