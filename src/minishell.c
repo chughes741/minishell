431684b87863606ad00530529d75d94d10dc7cf5
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-09-07 17:42:39 -0400

    Added signal handling for ctrl \, C, and D

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: chughes <chughes@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 11:30:00 by chughes           #+#    #+#             */
-/*   Updated: 2022/09/07 17:10:33 by chughes          ###   ########.fr       */
+/*   Updated: 2022/09/07 17:41:37 by chughes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,21 @@
 
 void	signal_handler(int signal)
 {
-	(void)signal;
-	return ;
+	if (signal == SIGINT)
+		return ;
+	if (signal == SIGQUIT)
+		return ;
+	exit(0);
 }
 
 int	main()
 {
 	char	*cmd;
 
-	cmd = ft_strdup("Welcome to Marc and Cole's minishell");
 	signal(SIGINT, signal_handler);
+	signal(SIGSEGV, signal_handler);
+	signal(SIGQUIT, signal_handler);
+	cmd = ft_strdup("Welcome to Marc and Cole's minishell");
 	while (ft_strncmp(cmd, "exit", 4))
 	{
 		free(cmd);
