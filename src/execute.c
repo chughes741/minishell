5bc4ecec599dfd8f009d1134f8bd7b911696c815
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-09-08 14:04:13 -0400

    Fixed typo

--- src/execute.c
+++ src/execute.c
@@ -6,7 +6,7 @@
 /*   By: chughes <chughes@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 12:42:42 by chughes           #+#    #+#             */
-/*   Updated: 2022/09/08 12:43:02 by chughes          ###   ########.fr       */
+/*   Updated: 2022/09/08 14:04:04 by chughes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	exe(t_params *params)
 	if (id != 0)
 		return (0);
 	dup2(params->fd_in, STDIN_FILENO);
-	dup2(params->fd_in, STDOUT_FILENO);
+	dup2(params->fd_out, STDOUT_FILENO);
 	execve(params->path, params->exec_arg, params->envp);
 	return (1);
 }
