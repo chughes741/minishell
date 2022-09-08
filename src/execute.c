5b74efc907ed3bc47bcce3af03509cb771106ffc
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-09-08 17:11:14 -0400

    Added waitpid to exe

--- src/execute.c
+++ src/execute.c
@@ -6,7 +6,7 @@
 /*   By: chughes <chughes@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 12:42:42 by chughes           #+#    #+#             */
-/*   Updated: 2022/09/08 14:04:04 by chughes          ###   ########.fr       */
+/*   Updated: 2022/09/08 14:05:46 by chughes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,14 @@
 // Executes program specified in params
 int	exe(t_params *params)
 {
-	int	id;
+	pid_t	id;
 
 	id = fork();
 	if (id != 0)
+	{
+		waitpid(id, NULL, 0);
 		return (0);
+	}
 	dup2(params->fd_in, STDIN_FILENO);
 	dup2(params->fd_out, STDOUT_FILENO);
 	execve(params->path, params->exec_arg, params->envp);
