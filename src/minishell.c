c62892ad00d8b84905004fa8b0f3d144863abfb1
Cole Hughes <95713039+chughes741@users.noreply.github.com>
2022-09-29 18:02:38 -0400

    Removed i in favour of data->n_cmds

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: chughes <chughes@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 11:30:00 by chughes           #+#    #+#             */
-/*   Updated: 2022/09/29 17:43:45 by chughes          ###   ########.fr       */
+/*   Updated: 2022/09/29 18:01:55 by chughes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 int	main(int argc, char *argv[], char *envp[])
 {
 	t_data	*data;
-	int		i;
 
 	data = get_data();
 	init_data(envp);
@@ -23,16 +22,14 @@ int	main(int argc, char *argv[], char *envp[])
 		data->run = false;
 	while (data->run == true)
 	{
-		data->n_cmds = 0;
 		data->last_cmd = prompter();
 		data->params = parse_args(data->last_cmd);
-		i = -1;
-		while (++i < data->n_cmds)
+		while (--data->n_cmds >= 0)
 		{
-			if (run_builtin(data->params[i]) == false)
+			if (run_builtin(data->params[data->n_cmds]) == false)
 			{
-				exe(data->params[i]);
-				waitpid(data->params[i]->id, &data->exit_status, 0);
+				exe(data->params[data->n_cmds]);
+				waitpid(data->params[data->n_cmds]->id, &data->exit_status, 0);
 				// if (WIFEXITED(data->exit_status) != 0)
 					// error_handler();
 			}
