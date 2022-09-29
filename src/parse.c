57973c2eefa9fe140cbf45ce8d08167592787b65
Cole Hughes <95713039+chughes741@users.noreply.github.com>
2022-09-29 17:32:57 -0400

    Added dupes for cmd_parse to run simply

--- src/parse.c
+++ src/parse.c
@@ -6,7 +6,7 @@
 /*   By: chughes <chughes@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 17:46:23 by chughes           #+#    #+#             */
-/*   Updated: 2022/09/29 17:02:42 by chughes          ###   ########.fr       */
+/*   Updated: 2022/09/29 17:31:51 by chughes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,12 @@
 
 t_params	**parse_args(char *cmd)
 {
-	(void)cmd;
-	return (NULL);
+	t_data		*data;
+	t_params	**params;
+
+	data = get_data();
+	params = ft_calloc(2, sizeof(t_params*));
+	params[0] = cmd_parse(cmd);
+	data->n_cmds = 1;
+	return (params);
 }
