412ff718db3f82647b828bdd015cba787ebcac22
Cole Hughes <95713039+chughes741@users.noreply.github.com>
2022-09-29 18:31:17 -0400

    Added some basic multi command parsing

--- src/parse.c
+++ src/parse.c
@@ -6,7 +6,7 @@
 /*   By: chughes <chughes@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 17:46:23 by chughes           #+#    #+#             */
-/*   Updated: 2022/09/29 18:01:47 by chughes          ###   ########.fr       */
+/*   Updated: 2022/09/29 18:29:24 by chughes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,20 @@ t_params	**parse_args(char *cmd)
 {
 	t_data		*data;
 	t_params	**params;
+	char		**cmds;
+	int			i;
 
 	data = get_data();
-	params = ft_calloc(2, sizeof(t_params*));
-	params[0] = cmd_parse(cmd);
-	data->n_cmds = 1;
+	cmds = ft_split(cmd, '|');
+	data->n_cmds = 0;
+	while (cmds[data->n_cmds])
+		++data->n_cmds;
+	params = ft_calloc(data->n_cmds + 1, sizeof(t_params*));
+	i = 0;
+	while (cmds[i])
+	{
+		params[i] = cmd_parse(cmds[i]);
+		i++;
+	}
 	return (params);
 }
