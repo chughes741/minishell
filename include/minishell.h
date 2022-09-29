df08adb055afe832eb70a2bbe228bd057ce82025
Cole Hughes <95713039+chughes741@users.noreply.github.com>
2022-09-29 17:23:37 -0400

    Added builtins to header

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: chughes <chughes@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 11:30:00 by chughes           #+#    #+#             */
-/*   Updated: 2022/09/29 17:11:48 by chughes          ###   ########.fr       */
+/*   Updated: 2022/09/29 17:23:28 by chughes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -121,7 +121,13 @@ void	*xrealloc(void *ptr, size_t size);
 
 // Builtins
 bool	run_builtin(t_params *params);
+void	builtin_echo(char *str);
+void	builtin_cd(char *new_dir);
 char	*builtin_pwd(void);
+void	builtin_export(char *new_var);
+void	builtin_unset(char *var_name);
+void	builtin_env(void);
+void	builtin_exit(void);
 
 
 #endif // MINISHELL_H
