16ad02f3d722099c299b28c19b3a5aa743b9e42c
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-09-30 11:56:06 -0400

    Norm formatting

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: chughes <chughes@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 11:30:00 by chughes           #+#    #+#             */
-/*   Updated: 2022/09/30 11:45:38 by chughes          ###   ########.fr       */
+/*   Updated: 2022/09/30 11:55:00 by chughes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 # define MINISHELL_H
 
 // STDLIB Includes
-# include <unistd.h> // write, access, read, close, fork, getcwd, chdir, unlink, execve, dup, dup2, pipe, isatty, ttyname, ttyslot
+# include <unistd.h> // write, access, read, close, fork, getcwd, chdir, unlink,
+					//execve, dup, dup2, pipe, isatty, ttyname, ttyslot
 # include <stdio.h> // readline, printf, perror
 # include <stdlib.h> // malloc, free, exit, getenv, 
 # include <readline/readline.h> // readline
@@ -33,14 +34,13 @@
 
 // Extra includes
 # include <errno.h> // errno
-# include <stdbool.h>
+# include <stdbool.h> // true, false, bool
 
 // Libft
 # include "../libft/include/libft.h"
 
 // Prompt Macro
-#define PROMPT "> "
-
+# define PROMPT "> "
 
 // Struct for storing initialization info for exec
 typedef struct s_params {
@@ -66,28 +66,27 @@ typedef struct s_data {
 	int			error_log_fd;
 }		t_data;
 
-
 // Data struct functions
-void	init_data(char	*envp[]);
-t_data	*get_data(void);
-void	del_data(void);
+void		init_data(char	*envp[]);
+t_data		*get_data(void);
+void		del_data(void);
 
 // Error handling
-void	error_handler(void);
+void		error_handler(void);
 
 // Signal handling
-void	init_signals(void);
-void	handle_interupt(int signum);
-void	handle_quit(int signum);
-void	handle_abort(int signum);
+void		init_signals(void);
+void		handle_interupt(int signum);
+void		handle_quit(int signum);
+void		handle_abort(int signum);
 
 // Parsing functions
 t_params	**parse_args(char *cmd);
 
 // Runtime loop functions
-char	*prompter(void);
-void	log_history(char *cmd);
-void	load_history(void);
+char		*prompter(void);
+void		log_history(char *cmd);
+void		load_history(void);
 
 // Exec setup functions
 char		**split_paths(char *envp[]);
@@ -96,24 +95,23 @@ int			exe(t_params *params);
 t_params	*cmd_parse(char *line);
 
 // I/O functions
-void	open_pipe(void);
-void	here_doc_filler(int output_fd, char *key);
-void	write_str(char *str, int fd);
+void		open_pipe(void);
+void		here_doc_filler(int output_fd, char *key);
+void		write_str(char *str, int fd);
 
 // Standard Library like functions
-char	*strcdup(char *basestr, char *matchchrs);
-void	*xfree(void *ptr);
-void	*xrealloc(void *ptr, size_t size);
+char		*strcdup(char *basestr, char *matchchrs);
+void		*xfree(void *ptr);
+void		*xrealloc(void *ptr, size_t size);
 
 // Builtins
-bool	run_builtin(t_params *params);
-void	builtin_echo(char *str);
-void	builtin_cd(char *new_dir);
-char	*builtin_pwd(void);
-void	builtin_export(char *new_var);
-void	builtin_unset(char *var_name);
-void	builtin_env(void);
-void	builtin_exit(void);
-
+bool		run_builtin(t_params *params);
+void		builtin_echo(char *str);
+void		builtin_cd(char *new_dir);
+char		*builtin_pwd(void);
+void		builtin_export(char *new_var);
+void		builtin_unset(char *var_name);
+void		builtin_env(void);
+void		builtin_exit(void);
 
 #endif // MINISHELL_H
