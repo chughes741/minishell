b1a50d91f53dbb2e9f902824f21d20989f492905
Cole Hughes <95713039+chughes741@users.noreply.github.com>
2022-10-09 17:54:28 -0400

    Added signal init

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: chughes <chughes@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 11:30:00 by chughes           #+#    #+#             */
-/*   Updated: 2022/10/06 21:30:02 by chughes          ###   ########.fr       */
+/*   Updated: 2022/10/09 17:46:55 by chughes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ int	wait_all(t_data	*data)
 	i = 0;
 	while (++i < data->n_cmds)
 	{
-		waitpid(data->params[i]->id, &exit_status, 0);
+		if (cmd_index(data->params[i]->exec_arg[0]) == 8)
+			waitpid(data->params[i]->id, &exit_status, 0);
 		++i;
 	}
 	return (exit_status);
@@ -36,6 +37,7 @@ int	main(int argc, char *argv[], char *envp[])
 	init_data(envp);
 	if (argc != 1 || argv[0] == NULL)
 		data->run = false;
+	init_signals();
 	while (data->run == true)
 	{
 		data->n_cmds = 0;
@@ -47,7 +49,7 @@ int	main(int argc, char *argv[], char *envp[])
 		{
 			data->run_cmd[cmd_index(data->params[i]->exec_arg[0])](data->params[i]);
 			i++;
-		}
+		} //TODO wc isn't hanging, prompt is just returned
 		data->exit_status = wait_all(data);
 		free_params(data->params);
 	}
