3a3ecab470e51de9636d52ef68c674b637583580
Cole Hughes <95713039+chughes741@users.noreply.github.com>
2022-10-10 20:34:22 -0400

    Added sig_int code

--- src/signals.c
+++ src/signals.c
@@ -6,25 +6,28 @@
 /*   By: chughes <chughes@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 12:05:28 by chughes           #+#    #+#             */
-/*   Updated: 2022/10/10 20:19:09 by chughes          ###   ########.fr       */
+/*   Updated: 2022/10/10 20:25:46 by chughes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-// Handles signals
+// Handles interupt signal "^C"
 void	sig_int_handler(int signum)
 {
+	//TODO add child killer
 	(void)signum;
+	ft_putchar_fd('\n', STDOUT_FILENO);
+	rl_on_new_line();
+	rl_redisplay();
     return ;
 }
 
-// Handles quit signal
+// Handles quit signal "^\"
 void	sig_quit_handler(int signum)
 {
 	(void)signum;
 	rl_redisplay();
-	ft_putstr_fd("  \b\b", STDOUT_FILENO);
 	return ;
 }
 
