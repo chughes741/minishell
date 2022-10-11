405703bb0e0cdcb3033e28eb5e7321294869bb63
Cole Hughes <95713039+chughes741@users.noreply.github.com>
2022-10-10 20:19:59 -0400

    Fixed sig quit

--- src/signals.c
+++ src/signals.c
@@ -6,7 +6,7 @@
 /*   By: chughes <chughes@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 12:05:28 by chughes           #+#    #+#             */
-/*   Updated: 2022/10/10 15:20:55 by chughes          ###   ########.fr       */
+/*   Updated: 2022/10/10 20:19:09 by chughes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@ void	sig_int_handler(int signum)
 void	sig_quit_handler(int signum)
 {
 	(void)signum;
-	rl_replace_line("", 0);
 	rl_redisplay();
+	ft_putstr_fd("  \b\b", STDOUT_FILENO);
 	return ;
 }
 
