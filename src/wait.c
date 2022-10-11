7b3422660a4c9db661a7bd2ca68a9b3e09a0ebb1
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-10-11 10:23:25 -0400

    Norm error

--- src/wait.c
+++ src/wait.c
@@ -6,7 +6,7 @@
 /*   By: chughes <chughes@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 10:20:57 by chughes           #+#    #+#             */
-/*   Updated: 2022/10/11 10:21:07 by chughes          ###   ########.fr       */
+/*   Updated: 2022/10/11 10:22:36 by chughes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 // Waits for all children to return
 void	wait_all(t_data	*data)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (i < data->n_cmds)
