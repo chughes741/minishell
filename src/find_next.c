edbe98922a23d162a3f1ae3664aa62bdf1a0b354
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-10-13 12:13:52 -0400

    Fixed norm errors in find next

--- src/find_next.c
+++ src/find_next.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   find_next.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: malord <malord@student.42quebec.com>       +#+  +:+       +#+        */
+/*   By: chughes <chughes@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 15:04:23 by chughes           #+#    #+#             */
-/*   Updated: 2022/10/12 15:42:54 by malord           ###   ########.fr       */
+/*   Updated: 2022/10/13 12:13:10 by chughes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 // Returns index of next chr match in str
 int	find_next(char *str, char *chr)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	while(str[i] && ft_strchr(chr, str[i]) == NULL)
+	while (str[i] && ft_strchr(chr, str[i]) == NULL)
 		++i;
 	return (i);
 }
