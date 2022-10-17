c630304db916a322f575752f7403a225bd5966ab
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-10-17 11:50:05 -0400

    Fixed too large alloc for cmd names

--- src/data.c
+++ src/data.c
@@ -6,7 +6,7 @@
 /*   By: chughes <chughes@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 11:58:01 by chughes           #+#    #+#             */
-/*   Updated: 2022/10/17 11:09:52 by chughes          ###   ########.fr       */
+/*   Updated: 2022/10/17 11:49:32 by chughes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@ char	**init_cmd_names(void)
 {
 	char	**cmd_names;
 
-	cmd_names = (char **)ft_calloc(9, sizeof(char *));
+	cmd_names = (char **)ft_calloc(8, sizeof(char *));
 	cmd_names[0] = ft_strdup("echo");
 	cmd_names[1] = ft_strdup("cd");
 	cmd_names[2] = ft_strdup("pwd");
