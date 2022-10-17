9d09cc0e2d28574d2e8bf2698ebbffddb1d6112e
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-10-17 11:51:15 -0400

    Fixed index error

--- src/wait.c
+++ src/wait.c
@@ -6,7 +6,7 @@
 /*   By: chughes <chughes@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 10:20:57 by chughes           #+#    #+#             */
-/*   Updated: 2022/10/17 10:47:42 by chughes          ###   ########.fr       */
+/*   Updated: 2022/10/17 11:50:57 by chughes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	wait_all(t_data	*data)
 	i = 0;
 	while (i < data->n_cmds)
 	{
-		if (cmd_idx(data->params[i]->exec_arg[0]) == 8)
+		if (cmd_idx(data->params[i]->exec_arg[0]) == 7)
 			waitpid(data->params[i]->id, &data->exit_status, 0);
 		++i;
 	}
